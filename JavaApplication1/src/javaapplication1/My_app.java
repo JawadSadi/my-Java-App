dd8f5f4e917409b00240002c640da72378327d58
Jawad <Jawadsadiq7373@gmail.com>
2024-07-18 00:51:52 -0700

    it is the design of my application

---
+++ JavaApplication1/src/javaapplication1/My_app.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+import login.loginFrame;
+
+import home.HomeFrame;
+
+/**
+ *
+ * @author hi
+ */
+public class My_app {
+    
+    public static void main(String[]args){
+ 
+//        loginFrame loginObj = new loginFrame();
+//        loginObj.setVisible(true);
+          HomeFrame homeObj = new HomeFrame();
+          homeObj.setVisible(true);
+}
+    
+}
